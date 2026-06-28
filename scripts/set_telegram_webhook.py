d03851f7d1082f035df58e266e34a51457c65b73
anaseous <64007680+anaseous@users.noreply.github.com>
2026-06-28 18:41:17 +0400

    Delete scripts directory

--- scripts/set_telegram_webhook.py
+++
@@ -1,6 +0,0 @@
-import os, requests
-from dotenv import load_dotenv
-load_dotenv()
-TOKEN=os.getenv('TELEGRAM_BOT_TOKEN'); PUBLIC_URL=os.getenv('APP_PUBLIC_URL','').rstrip('/'); SECRET=os.getenv('WEBHOOK_SECRET')
-r=requests.post(f'https://api.telegram.org/bot{TOKEN}/setWebhook',json={'url':f'{PUBLIC_URL}/telegram/{SECRET}','drop_pending_updates':True},timeout=20)
-print(r.status_code); print(r.text)
